f23cbd1e3d82147144abd9b3bd8c1f99447c855c
Tijn Schuurmans <tijn@tijnschuurmans.nl>
2014-04-04 23:25:25 +0200

    put everything in a class

--- lib/watcher_in_the_water.rb
+++ lib/watcher_in_the_water.rb
@@ -10,7 +10,6 @@ require 'digest/sha1'
 require 'fileutils'
 
 module WatcherInTheWater
-
   HELP = <<-end_help
 You need {{config}} to contain a sender jabber ID, password,
 a recipient jabber ID, and a list of URLs in YAML format. Example:
@@ -22,68 +21,67 @@ recipient: phil@hagelb.org
 urls: ---
   http://rubyconf.org
 end_help
-
-
-  include Jabber
   VERSION = '0.1'
 
-  module_function
+  class Watcher
+    include Jabber
 
-  def configure(config_file = nil)
-    config_file ||= "#{XDG['CONFIG_HOME']}/watcher/config.yml"
-    filename = File.expand_path(config_file)
-    @config = YAML.load_file(filename)
-  rescue
-    abort HELP.gsub("{{config}}", filename)
-  end
-
-  def urls
-    @urls ||= load_urls
-  end
-
-  def watch
-    urls.each do |url|
-      watch_url(url)
+    def initialize(config_file = nil)
+      config_file ||= "#{XDG['CONFIG_HOME']}/watcher/config.yml"
+      filename = File.expand_path(config_file)
+      @config = YAML.load_file(filename)
+    rescue
+      abort HELP.gsub("{{config}}", filename)
     end
-  end
 
-  def watch_url(url)
-    content = open(url).read
-    hash = Digest::SHA1.hexdigest(content)
-    filename = 'hashes/' + url_transform(url)
+    def urls
+      @urls ||= load_urls
+    end
 
-    if File.exist?(filename)
-      if File.read(filename) != hash
-        alert("#{url} changed to #{content[0 .. 200]}")
+    def watch
+      urls.each do |url|
+        watch_url(url)
       end
-    else
-      alert("Watching #{url} for you!")
     end
-    FileUtils.mkdir_p(File.dirname(filename))
-    File.open(filename, 'w') { |f| f.write hash }
-  end
 
-  def alert(message)
-    connect
-    @client.send Message.new(@config['recipient'],
-                             message).set_type(:normal).set_id('1')
-  end
+    def watch_url(url)
+      content = open(url).read
+      hash = Digest::SHA1.hexdigest(content)
+      filename = 'hashes/' + url_transform(url)
 
-  def connect
-    return if @client
-    jid = JID.new("#{@config['jid']}/#{`hostname`.chomp}-watcher")
-    @client = Client.new(jid)
-    @client.connect
-    @client.auth(@config['password'])
-  end
+      if File.exist?(filename)
+        if File.read(filename) != hash
+          alert("#{url} changed to #{content[0 .. 200]}")
+        end
+      else
+        alert("Watching #{url} for you!")
+      end
+      FileUtils.mkdir_p(File.dirname(filename))
+      File.open(filename, 'w') { |f| f.write hash }
+    end
 
-  def url_transform(url)
-    url.gsub(/[^\w]/, '_')[0 .. 50] + '_' + Digest::SHA1.hexdigest(url)
-  end
+    def alert(message)
+      connect
+      @client.send Message.new(@config['recipient'],
+                               message).set_type(:normal).set_id('1')
+    end
 
-  def load_urls
-    Dir.glob("#{XDG['CONFIG_HOME']}/watcher/urls/*.yml").map do |filename|
-      YAML.load_file(filename)
+    def connect
+      return if @client
+      jid = JID.new("#{@config['jid']}/#{`hostname`.chomp}-watcher")
+      @client = Client.new(jid)
+      @client.connect
+      @client.auth(@config['password'])
+    end
+
+    def url_transform(url)
+      url.gsub(/[^\w]/, '_')[0 .. 50] + '_' + Digest::SHA1.hexdigest(url)
+    end
+
+    def load_urls
+      Dir.glob("#{XDG['CONFIG_HOME']}/watcher/urls/*.yml").map do |filename|
+        YAML.load_file(filename)
+      end
     end
   end
 end
